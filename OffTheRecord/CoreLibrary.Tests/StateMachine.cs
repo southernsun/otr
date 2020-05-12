a219c366d37a5cc07645f74d293ee057e6d9fc9b
southernsun <southernsun@users.noreply.github.com>
2020-05-12 22:05:09 +0200

    Converted CoreLib.Tests into CoreLibrary.Tests (correct framework) and fixed unit test.

---
+++ OffTheRecord/CoreLibrary.Tests/StateMachine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OffTheRecord.CoreLibrary;
+using OffTheRecord.CoreLibrary.internals;
+
+namespace CoreLib.Tests
+{
+    [TestClass]
+    public class StateMachineTests
+    {
+        [TestMethod]
+        public void RunSampleStateMachine()
+        {
+            var stateMachineDefinition = StateMachineUtils.Parse("samplestatemachine.json");
+            var state = new StateMachine(stateMachineDefinition, new object());
+
+        }
+    }
+}
---
+++ OffTheRecord/CoreLibrary/internals/StateMachineUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace OffTheRecord.CoreLibrary.internals
+{
+    public class StateMachineUtils
+    {   
+        public static StateMachineDefinition Parse(string jsonFile) {
+            return JsonConvert.DeserializeObject<StateMachineDefinition>(File.ReadAllText(jsonFile));
+        }
+    }
+}
