6da8c21efa21294da5175b046666ed06f6399ddf
southernsun <southernsun@users.noreply.github.com>
2020-05-13 22:34:13 +0200

    Added OtrNonce

---
+++ OffTheRecord/CoreLibrary/DataTypes/OtrNonce.cs
@@ -0,0 +1,17 @@
+﻿namespace OffTheRecord.CoreLibrary.DataTypes
+{
+    public readonly struct OtrNonce
+    {
+        public byte[] Value
+        {
+            get
+            {
+                return new byte[12] { 
+                    0,0,0,0,
+                    0,0,0,0,
+                    0,0,0,0
+                };
+            }
+        }
+    }
+}
