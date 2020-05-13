8ce1d8e65f4d4a876ccc061bf194ebe202241c31
southernsun <southernsun@users.noreply.github.com>
2020-05-13 11:37:21 +0200

    InstanceTag test.

---
+++ OffTheRecord/CoreLibrary.Tests/InstanceTagTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OffTheRecord.CoreLibrary.Classes;
+
+namespace CoreLibrary.Tests
+{
+    [TestFixture]
+    public class InstanceTagTest
+    {
+        [Test]
+        public void IsValidShouldReturnFalseBetween0and100Hex([Range(1, 255, 1)] int x)
+        {
+            uint value = (uint) x;
+            InstanceTag.IsValid(value).Should().BeFalse();
+        }
+
+        [Test]
+
+        public void IsValid()
+        {
+            InstanceTag.IsValid(0).Should().BeTrue();
+            InstanceTag.IsValid(256).Should().BeTrue();
+        }
+    }
+}
---
+++ OffTheRecord/CoreLibrary/Classes/InstanceTag.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OffTheRecord.CoreLibrary.Classes
+{
+    public class InstanceTag 
+    {
+        ////private uint instanceTag; // 4 byte unsigned int to hold instance tag.
+
+        public static bool IsValid(uint instanceTag)
+        {
+            // hex = 100 is 256 in decimal;
+            if (instanceTag > 0 && instanceTag < 256)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
