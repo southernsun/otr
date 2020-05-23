02c0e09d01aba91a16705a5cce2191d36116402b
southernsun <southernsun@users.noreply.github.com>
2020-05-23 20:40:45 +0200

    Improved testing of base datatypes.

--- OffTheRecord/CoreLibrary.Tests/DataTypeTest.cs
+++ OffTheRecord/CoreLibrary.Tests/DataTypeTest.cs
@@ -8,33 +8,72 @@ namespace CoreLibrary.Tests
     public class DataTypeTest
     {
         [Test]
-        public void DataTypeOtrIntTests()
+        public void DataTypeOtrIntShouldWorkAsAnInt()
         {
             OtrInt int1 = 4;
             int1.Should().Be(4);
+        }
 
+        [Test]
+        public void DataTypeOtrIntShouldSupportMaxValueOfUnassignedInt()
+        {
             OtrInt int2 = uint.MaxValue;
             int2.Should().Be(uint.MaxValue);
         }
 
         [Test]
-        public void DataTypeOtrByteTests()
+        public void DataTypeOtrIntShouldMatchOnComparison()
+        {
+            OtrInt int3 = 2;
+            OtrInt int4 = 2;
+
+            int3.Should().Be(int4);
+        }
+
+        [Test]
+        public void DataTypeOtrByteShouldWorkAsAnByte()
         {
             OtrByte byte1 = 4;
             byte1.Should().Be(4);
+        }
 
+        [Test]
+        public void DataTypeOtrByteShouldSupportMaxValueOfByte()
+        {
             OtrByte byte2 = byte.MaxValue;
             byte2.Should().Be(byte.MaxValue);
         }
 
         [Test]
-        public void DataTypeOtrShortTests()
+        public void DataTypeOtrByteShouldMatchOnComparison()
+        {
+            OtrByte byte3 = 2;
+            OtrByte byte4 = 2;
+
+            byte3.Should().Be(byte4);
+        }
+
+        [Test]
+        public void DataTypeOtrShortShouldWorkAsAnShort()
         {
             OtrShort short1 = 4;
             short1.Should().Be(4);
+        }
 
+        [Test]
+        public void DataTypeOtrShortShouldSupportMaxValueOfUnassignedShort()
+        {
             OtrShort short2 = ushort.MaxValue;
             short2.Should().Be(ushort.MaxValue);
+        }
+
+        [Test]
+        public void DataTypeOtrshortShouldMatchOnComparison()
+        {
+            OtrShort short3 = 2;
+            OtrShort short4 = 2;
+
+            short3.Should().Be(short4);
         }
     }
 }
