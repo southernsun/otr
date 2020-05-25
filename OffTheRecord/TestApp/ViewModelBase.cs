0f3d52f032641967df6b0604aa84139cda22cf72
BUTEAU Christopher <CBU1@dsone.3ds.com>
2020-05-24 20:11:20 -0400

    starting test XAML app

---
+++ OffTheRecord/TestApp/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TestApp
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string prop = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ OffTheRecord/TestApp/vms/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApp.vms
+{
+    public class MainWindow : ViewModelBase
+    {
+
+        private string _content;
+        public string Content
+
+        {
+            get { return _content; }
+            set
+            {
+                _content = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
