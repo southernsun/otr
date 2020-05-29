fd5667da9a0cf1a520dfd6a54d215685df2c98bb
southernsun <southernsun@users.noreply.github.com>
2020-05-29 22:49:44 +0200

    Added Example Specflow Test project

---
+++ OffTheRecord/SpecFlowTests/SpecFlowFeature1.feature
@@ -0,0 +1,7 @@
+﻿Feature: SpecFlowFeature1
+
+Scenario: Add two numbers
+       Given I have entered 50 into the calculator
+       And I have also entered 70 into the calculator
+       When I press add
+       Then the result should be 120 on the screen
---
+++ OffTheRecord/SpecFlowTests/SpecFlowFeature1Steps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTests
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
